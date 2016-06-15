4341d1d4f4ba30aee259cb4f5248d77f3f735ef5
Daniel Gonçalves <1151452@isep.ipp.pt>
2016-06-15 01:36:00 +0100

    Refactor get submittable lists functionality [Refactor] [UC-13] [fixes issue #65]

--- src/main/java/lapr/project/controller/DetectConflictsController.java
+++ src/main/java/lapr/project/controller/DetectConflictsController.java
@@ -5,9 +5,12 @@ package lapr.project.controller;
 
 import java.util.List;
 import lapr.project.model.Application;
-import lapr.project.model.Conflict;
+import lapr.project.model.ApplicationsList;
 import lapr.project.model.ConflictType;
+import lapr.project.model.ConflictTypesRegister;
+import lapr.project.model.ConflictsList;
 import lapr.project.model.ExhibitionCenter;
+import lapr.project.model.StaffList;
 import lapr.project.model.StaffMember;
 import lapr.project.model.Submittable;
 
@@ -23,59 +26,23 @@ import lapr.project.model.Submittable;
 public class DetectConflictsController {
 
     /**
-     * The exhibition center.
-     */
-    private final ExhibitionCenter exhibitionCenter;
-
-    /**
-     * The submittable which the timer was triggered.
-     */
-    private final Submittable submittable;
-
-    /**
      * Constructs a DetectConflictsController Class.
      *
      * @param exhibitionCenter Exhibition Center
      * @param submittable Submittable which the timer was triggered
      */
     public DetectConflictsController(ExhibitionCenter exhibitionCenter, Submittable submittable) {
-        this.exhibitionCenter = exhibitionCenter;
-        this.submittable = submittable;
+
+        ConflictTypesRegister conflictTypesRegister = exhibitionCenter.getConflictTypesRegister();
+        List<ConflictType> conflictTypesList = conflictTypesRegister.getConflictTypesList();
+
+        ApplicationsList applicationsList = submittable.getApplicationsList();
+        List<Application> applications = applicationsList.getApplicationsList();
+
+        StaffList staffList = submittable.getStaffList();
+        List<StaffMember> staff = staffList.getStaffList();
+
+        ConflictsList conflictsList = submittable.getConflictsList();
     }
 
-    /**
-     * Obtain the conflict types list.
-     *
-     * @return the conflict types list
-     */
-    public List<ConflictType> getConflictTypesList() {
-        return this.exhibitionCenter.getConflictTypesRegister().getConflictTypesList();
-    }
-
-    /**
-     * Obtain the submittable applications list.
-     *
-     * @return the submittable applications list
-     */
-    public List<Application> getApplicationsList() {
-        return this.submittable.getApplicationsList().getApplicationsList();
-    }
-
-    /**
-     * Obtain the submittable staff list.
-     *
-     * @return the submittable staff list
-     */
-    public List<StaffMember> getStaffList() {
-        return this.submittable.getStaffList().getStaffList();
-    }
-
-    /**
-     * Obtain the submittable conflicts list.
-     *
-     * @return the submittable conflicts list
-     */
-    public List<Conflict> getConflictsList() {
-        return this.submittable.getConflictsList().getConflictsList();
-    }
 }
