1f83a0635916e45379472ed736d82234a13e05fd
Renato <1140822@isep.ipp.pt>
2016-06-22 11:37:06 +0100

    Beginning login implementation add class ExhibitionsManagerRegister [implementation] [UI] [fixes issue #79]

--- src/main/java/lapr/project/ui/Main.java
+++ src/main/java/lapr/project/ui/Main.java
@@ -1,81 +1,48 @@
+/**
+ * Package location for UI concepts.
+ */
 package lapr.project.ui;
 
-//import java.io.File;
-//import javax.xml.bind.JAXBContext;
-//import javax.xml.bind.Marshaller;
-//import javax.xml.bind.Unmarshaller;
-//import lapr.project.model.Exhibition;
-//import lapr.project.model.ExhibitionCenter;
-//import lapr.project.utils.DefaultInstantiator;
+import java.util.List;
+import lapr.project.model.ExhibitionCenter;
+import lapr.project.model.ExhibitionsManager;
+import lapr.project.model.ExhibitionsManagerRegister;
 
 /**
- * @author Nuno Bettencourt <nmb@isep.ipp.pt> on 24/05/16.
+ * Represents a applications list
+ *
+ * @author Daniel Gonçalves 1151452
+ * @author Eric Amaral 1141570
+ * @author Ivo Ferro 1151159
+ * @author Renato Oliveira 1140822
+ * @author Ricardo Correia 1151231
  */
 class Main {
 
     /**
      * Private constructor to hide implicit public one.
      */
-    private Main() {
+    private Main(ExhibitionCenter exhibitionCenter) {
+        ExhibitionsManagerRegister exhibitionManagerRegister = exhibitionCenter.getExhibitionsManagerRegister();
 
     }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-//		CalculatorExample calculatorExample = new CalculatorExample();
-//		System.out.println(calculatorExample.sum(3, 5));
-//                                     Exhibition e = new Exhibition();
-//                                      
-//                                     ExhibitionApplication a = new ExhibitionApplication();
-//                                     List<ExhibitionApplication> listApp = new ArrayList();
-//                                     listApp.add(a);
-//                                  
-//                                     StaffAttributionsList staff = new StaffAttributionsList();
-//                                     StaffAttribution sf = new StaffAttribution();
-//                                     List<StaffAttribution> lstStaff = new ArrayList();
-//                                     lstStaff.add(sf);
-//                                      staff.setStaffAttributionsList(lstStaff);
-//                                     e.setStaffAttributionsList(staff);
-//                                     Demonstration b = new Demonstration();
-//                                     b.setStaffAttributionsList(staff);
-//                                     List<Demonstration> listDemos = new ArrayList();
-//                                     listDemos.add(b);
-//                                     DemonstrationsList demoList = new DemonstrationsList(listDemos);
-//                                     e.setDemonstrationsList(demoList);
-//                                     e.xmlexp();
-        
-        
-//        //ureg.xmlexp();
-//        ExhibitionCenter exhibitionCenter = DefaultInstantiator.createExhibitionCenter();
-//
-//        Exhibition exhibitionTest = exhibitionCenter.getExhibitionsRegister().getExhibitionsList().get(0);
-//        
-//        
-//        // create JAXB context and instantiate marshaller
-//        JAXBContext context = JAXBContext.newInstance(Exhibition.class);
-//        Marshaller m = context.createMarshaller();
-//        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-//
-//        // Write to System.out
-//        File xmlFile = new File("D:\\exhibition3.xml");
-//
-//        m.marshal(exhibitionTest, System.out);
-//
-//        m.marshal(exhibitionTest, xmlFile);
-//
-//        JAXBContext jaxbContext = JAXBContext.newInstance(Exhibition.class);
-        
-        
-        
-//
-//        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-//
-//        Exhibition exhibition = (Exhibition) jaxbUnmarshaller.unmarshal(xmlFile);
-//
-//        System.out.println(exhibition.equals(exhibitionTest));
+    }
+
+    public void openFrame(ExhibitionsManagerRegister exhibitionManagerRegister) {
+        List<ExhibitionsManager> exhibitionsManagerList = exhibitionManagerRegister.getExhibitionsManagerList();
+        if (exhibitionsManagerList.isEmpty()) {
+            //TODO: new user JFrame
+        } 
+        else 
+        {
+            //TODO: open login frame
+        }
     }
 
 }
