e61fa3caefabf8354278209ec14be86924ec6811
Daniel Gonçalves <1151452@isep.ipp.pt>
2016-06-23 01:40:12 +0100

    Implement the controller [Implementation] [UC-27] [fixes issue # 76]

--- src/main/java/lapr/project/controller/StaffEvaluationsAnalysisController.java
+++ src/main/java/lapr/project/controller/StaffEvaluationsAnalysisController.java
@@ -3,6 +3,11 @@
  */
 package lapr.project.controller;
 
+import java.util.List;
+import lapr.project.model.ExhibitionCenter;
+import lapr.project.model.Record;
+import lapr.project.model.StaffMemberAnalytic;
+
 /**
  * Represents the controller for a staff evaluations analysis.
  *
@@ -13,5 +18,31 @@ package lapr.project.controller;
  * @author Ricardo Correia 1151231
  */
 public class StaffEvaluationsAnalysisController {
-    
+
+    /**
+     * The exhibitions center
+     */
+    private final ExhibitionCenter exhibitionCenter;
+
+    /**
+     * Constructs a Staff Evaluations Analysis Controller.
+     *
+     * @param exhibitionCenter the exhibitions center
+     */
+    public StaffEvaluationsAnalysisController(ExhibitionCenter exhibitionCenter) {
+
+        this.exhibitionCenter = exhibitionCenter;
+    }
+
+    /**
+     * Calculates the staff evaluations analytics.
+     * 
+     * @return a list with the staff evaluations analytics
+     */
+    public List<StaffMemberAnalytic> getStaffAnalyticsList() {
+        
+        Record record = this.exhibitionCenter.getRecord();
+        
+        return record.calculateStaffAnalytics();
+    }
 }
